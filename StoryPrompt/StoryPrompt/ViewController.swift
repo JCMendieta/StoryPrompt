05bfe43cbf6bb69b9b53e451f3cdd5facd493a54
Andres Mendieta <carlos.camyoh@gmail.com>
2021-11-29 22:39:01 -0500

    Agregada la clase StoryPrompt y StoryPromptEntry , se crea un objeto storyPromptEntry en viewController

--- StoryPrompt/StoryPrompt/ViewController.swift
+++ StoryPrompt/StoryPrompt/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let storyPrompt = StoryPromptEntry()
+        storyPrompt.noun="toaster"
+        storyPrompt.adjective="smelly"
+        storyPrompt.verb="burps"
+        storyPrompt.number=10
+        print(storyPrompt)
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+ 
