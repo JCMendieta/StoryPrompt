efae427d04f7778bfcc73d7420ae08bfaca1f4f8
Andres Mendieta <carlos.camyoh@gmail.com>
2021-12-01 17:05:13 -0500

    Se agrega seleccion de imagenes en pantalla, se agrega segunda pantalla como pantalla inicial

---
+++ StoryPrompt/StoryPrompt/ViewControllers/StoryPromptViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StoryPromptViewController.swift
+//  StoryPrompt
+//
+//  Created by Andres Mendieta on 12/1/21.
+//
+
+import UIKit
+
+class StoryPromptViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
