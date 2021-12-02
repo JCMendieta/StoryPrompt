90b33c02f271fd98244a201951d8d94cd268d786
Andres Mendieta <carlos.camyoh@gmail.com>
2021-12-02 16:31:36 -0500

    Agregado navigation controller y segues entre StoryPromptController y AddStoryController ocultando la barra de navegacion

--- StoryPrompt/StoryPrompt/ViewControllers/StoryPromptViewController.swift
+++ StoryPrompt/StoryPrompt/ViewControllers/StoryPromptViewController.swift
@@ -24,15 +24,13 @@ class StoryPromptViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+   
 
 }
